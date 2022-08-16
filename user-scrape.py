dadbb1a2595111aedc7bd554f9a1bd3396a7c524
Blaze Alan Marshall <wertercatt@wertercatt.com>
2022-08-16 15:59:58 -0500

    Define scripts so I can call them from each other.

--- user-scrape.py
+++ user-scrape.py
@@ -8,22 +8,26 @@ import json
 def UGCClassFinder(tag):
     return tag.has_attr("data-publishedfileid")
 
+def scrape(SteamID64):
+    #URL Setup
+    BaseURL = "https://steamcommunity.com/profiles/"
+    Endpoint = "/myworkshopfiles/?p="
+    Page = 1
+    SubmissionIDs = []
+    #Get HTML
+    while Page < 1668:
+        EndpointRaw = requests.get(url = BaseURL + SteamID64 + Endpoint + str(Page))
+        EndpointHTML = EndpointRaw.text
+        #Parse HTML
+        HTMLParse = BeautifulSoup(EndpointHTML, "html.parser")
+        #find Submissions
+        WorkshopSubmissions = HTMLParse.find_all(UGCClassFinder)
+        for Submission in WorkshopSubmissions:
+            SubmissionIDs.append(Submission.get("data-publishedfileid"))
+        if len(WorkshopSubmissions) == 0:
+            break
+        Page += 1
+    print(json.dumps(SubmissionIDs))
 
-#URL Setup
-BaseURL = "https://steamcommunity.com/profiles/"
-SteamID64 = str(sys.argv[1])
-Endpoint = "/myworkshopfiles/?p="
-Page = 1
-#Get HTML
-while Page < 1668:
-    EndpointRaw = requests.get(url = BaseURL + SteamID64 + Endpoint + str(Page))
-    EndpointHTML = EndpointRaw.text
-    #Parse HTML
-    HTMLParse = BeautifulSoup(EndpointHTML, "html.parser")
-    #find Submissions
-    WorkshopSubmissions = HTMLParse.find_all(UGCClassFinder)
-    for Submission in WorkshopSubmissions:
-        print(Submission.get("data-publishedfileid"))
-    if len(WorkshopSubmissions) == 0:
-        break
-    Page += 1
+if __name__ == '__main__':
+    scrape(sys.argv[1])
--- workshop-downloader.py
+++ workshop-downloader.py
@@ -4,82 +4,86 @@ import datetime
 import requests
 import os
 
-#SteamWebAPI endpoints
-GetPublishedFileDetails = "https://api.steampowered.com/ISteamRemoteStorage/GetPublishedFileDetails/v1/"
-GetCollectionDetails = "https://api.steampowered.com/ISteamRemoteStorage/GetCollectionDetails/v1/"
-GetUGCFileDetails = "https://api.steampowered.com/ISteamRemoteStorage/GetUGCFileDetails/v1/"
+def download(WorkshopID):
+    #SteamWebAPI endpoints
+    GetPublishedFileDetails = "https://api.steampowered.com/ISteamRemoteStorage/GetPublishedFileDetails/v1/"
+    GetCollectionDetails = "https://api.steampowered.com/ISteamRemoteStorage/GetCollectionDetails/v1/"
+    GetUGCFileDetails = "https://api.steampowered.com/ISteamRemoteStorage/GetUGCFileDetails/v1/"
 
-#Load SteamWebAPI Key
-KeyFile = open("./SteamWebAPI.key", "r")
-Key = KeyFile.read()
-KeyFile.close()
+    #Load SteamWebAPI Key
+    KeyFile = open("./SteamWebAPI.key", "r")
+    Key = KeyFile.read()
+    KeyFile.close()
 
-#Load Workshop Page Details
-WorkshopID = str(sys.argv[1])
-GetPublishedFileDetailsData = {"key":Key,"itemcount":1,"publishedfileids[0]":WorkshopID}
-GetPublishedFileDetailsRaw = requests.post(url = GetPublishedFileDetails, data = GetPublishedFileDetailsData)
-PublishedFileDetails = json.loads(GetPublishedFileDetailsRaw.text)["response"]["publishedfiledetails"][0]
+    #Load Workshop Page Details
+    WorkshopID = str(sys.argv[1])
+    GetPublishedFileDetailsData = {"key":Key,"itemcount":1,"publishedfileids[0]":WorkshopID}
+    GetPublishedFileDetailsRaw = requests.post(url = GetPublishedFileDetails, data = GetPublishedFileDetailsData)
+    PublishedFileDetails = json.loads(GetPublishedFileDetailsRaw.text)["response"]["publishedfiledetails"][0]
 
-print(json.dumps(PublishedFileDetails, sort_keys=True, indent=4))
+    print(json.dumps(PublishedFileDetails, sort_keys=True, indent=4))
 
-#Get additional UGC metadata for the file
-CreatorAppID = str(PublishedFileDetails["creator_app_id"])
-GetUGCFileDetailsParametersFile = "?key=" + Key + "&ugcid=" + PublishedFileDetails["hcontent_file"] + "&appid=" + CreatorAppID
-GetUGCFileDetailsRawFile = requests.get(url = GetUGCFileDetails + GetUGCFileDetailsParametersFile)
-UGCFileDetailsFile = (json.loads(GetUGCFileDetailsRawFile.text)["data"])
+    #Get additional UGC metadata for the file
+    CreatorAppID = str(PublishedFileDetails["creator_app_id"])
+    GetUGCFileDetailsParametersFile = "?key=" + Key + "&ugcid=" + PublishedFileDetails["hcontent_file"] + "&appid=" + CreatorAppID
+    GetUGCFileDetailsRawFile = requests.get(url = GetUGCFileDetails + GetUGCFileDetailsParametersFile)
+    UGCFileDetailsFile = (json.loads(GetUGCFileDetailsRawFile.text)["data"])
 
-print(json.dumps(UGCFileDetailsFile, sort_keys=True, indent=4))
-
-
-#Get additional UGC metadata for the preview
-GetUGCFileDetailsParametersPreview = "?key=" + Key + "&ugcid=" + PublishedFileDetails["hcontent_preview"] + "&appid=" + CreatorAppID
-GetUGCFileDetailsRawPreview = requests.get(url = GetUGCFileDetails + GetUGCFileDetailsParametersPreview)
-UGCFileDetailsPreview = (json.loads(GetUGCFileDetailsRawPreview.text)["data"])
-
-print(json.dumps(UGCFileDetailsPreview, sort_keys=True, indent=4))
+    print(json.dumps(UGCFileDetailsFile, sort_keys=True, indent=4))
 
 
-#Get Variables for Output
-ConsumerAppID = str(PublishedFileDetails["consumer_app_id"])
-SteamID64 = PublishedFileDetails["creator"]
-DateCreated = datetime.datetime(1970, 1, 1, tzinfo=datetime.timezone.utc) + datetime.timedelta(seconds=PublishedFileDetails["time_created"])
-DateCreatedISO8601 = DateCreated.strftime("%Y-%m-%dT%H-%M-%S")
-WorkshopTitle = PublishedFileDetails["title"]
+    #Get additional UGC metadata for the preview
+    GetUGCFileDetailsParametersPreview = "?key=" + Key + "&ugcid=" + PublishedFileDetails["hcontent_preview"] + "&appid=" + CreatorAppID
+    GetUGCFileDetailsRawPreview = requests.get(url = GetUGCFileDetails + GetUGCFileDetailsParametersPreview)
+    UGCFileDetailsPreview = (json.loads(GetUGCFileDetailsRawPreview.text)["data"])
 
-#File Directory Variables
-FileName = UGCFileDetailsFile["filename"]
-FileDirectories = FileName.split("/")
-FileName = FileDirectories[-1]
-#Remove the actual FileName, leaving only the directories.
-FileDirectories.pop(-1)
+    print(json.dumps(UGCFileDetailsPreview, sort_keys=True, indent=4))
 
-#Preview Directory Variables
-PreviewName = UGCFileDetailsPreview["filename"]
-PreviewDirectories = PreviewName.split("/")
-PreviewName = PreviewDirectories[-1]
-#Remove the actual PreviewName, leaving only the directories.
-PreviewDirectories.pop(-1)
 
-#Output Directory Creation
-OutputDirectory = "./Workshop-Downloads/" + str(SteamID64) + "/" + ConsumerAppID + "/" + DateCreatedISO8601 + " - " + WorkshopTitle + "/"
-FileOutputDirectory = OutputDirectory
-for FileDirectory in FileDirectories:
-    FileOutputDirectory += FileDirectory + "/"
-PreviewOutputDirectory = OutputDirectory
-for PreviewDirectory in PreviewDirectories:
-    PreviewOutputDirectory += PreviewDirectory + "/"
-os.makedirs(OutputDirectory, exist_ok = True)
-os.makedirs(FileOutputDirectory, exist_ok = True)
-os.makedirs(PreviewOutputDirectory, exist_ok = True)
+    #Get Variables for Output
+    ConsumerAppID = str(PublishedFileDetails["consumer_app_id"])
+    SteamID64 = PublishedFileDetails["creator"]
+    DateCreated = datetime.datetime(1970, 1, 1, tzinfo=datetime.timezone.utc) + datetime.timedelta(seconds=PublishedFileDetails["time_created"])
+    DateCreatedISO8601 = DateCreated.strftime("%Y-%m-%dT%H-%M-%S")
+    WorkshopTitle = PublishedFileDetails["title"]
 
-#Download UGC Files
-os.system("wget -O \"" + FileOutputDirectory + FileName + "\" " + UGCFileDetailsFile["url"])
-os.system("wget -O \"" + PreviewOutputDirectory + PreviewName + "\" " + UGCFileDetailsPreview["url"])
+    #File Directory Variables
+    FileName = UGCFileDetailsFile["filename"]
+    FileDirectories = FileName.split("/")
+    FileName = FileDirectories[-1]
+    #Remove the actual FileName, leaving only the directories.
+    FileDirectories.pop(-1)
 
-#Save JSON Responses
-PublishedFileDetailsOutput = open(OutputDirectory + "/PublishedFileDetails.json", "w")
-json.dump(PublishedFileDetails, PublishedFileDetailsOutput, sort_keys=True, indent=4)
-UGCFileDetailsOutputFile = open(OutputDirectory + "/UGCFileDetails.file.json", "w")
-json.dump(UGCFileDetailsFile, UGCFileDetailsOutputFile, sort_keys=True, indent=4)
-UGCFileDetailsOutputPreview = open(OutputDirectory + "/UGCFileDetails.preview.json", "w")
-json.dump(UGCFileDetailsPreview, UGCFileDetailsOutputPreview, sort_keys=True, indent=4)
+    #Preview Directory Variables
+    PreviewName = UGCFileDetailsPreview["filename"]
+    PreviewDirectories = PreviewName.split("/")
+    PreviewName = PreviewDirectories[-1]
+    #Remove the actual PreviewName, leaving only the directories.
+    PreviewDirectories.pop(-1)
+
+    #Output Directory Creation
+    OutputDirectory = "./Workshop-Downloads/" + str(SteamID64) + "/" + ConsumerAppID + "/" + DateCreatedISO8601 + " - " + WorkshopTitle + "/"
+    FileOutputDirectory = OutputDirectory
+    for FileDirectory in FileDirectories:
+        FileOutputDirectory += FileDirectory + "/"
+    PreviewOutputDirectory = OutputDirectory
+    for PreviewDirectory in PreviewDirectories:
+        PreviewOutputDirectory += PreviewDirectory + "/"
+    os.makedirs(OutputDirectory, exist_ok = True)
+    os.makedirs(FileOutputDirectory, exist_ok = True)
+    os.makedirs(PreviewOutputDirectory, exist_ok = True)
+
+    #Download UGC Files
+    os.system("wget -O \"" + FileOutputDirectory + FileName + "\" " + UGCFileDetailsFile["url"])
+    os.system("wget -O \"" + PreviewOutputDirectory + PreviewName + "\" " + UGCFileDetailsPreview["url"])
+
+    #Save JSON Responses
+    PublishedFileDetailsOutput = open(OutputDirectory + "/PublishedFileDetails.json", "w")
+    json.dump(PublishedFileDetails, PublishedFileDetailsOutput, sort_keys=True, indent=4)
+    UGCFileDetailsOutputFile = open(OutputDirectory + "/UGCFileDetails.file.json", "w")
+    json.dump(UGCFileDetailsFile, UGCFileDetailsOutputFile, sort_keys=True, indent=4)
+    UGCFileDetailsOutputPreview = open(OutputDirectory + "/UGCFileDetails.preview.json", "w")
+    json.dump(UGCFileDetailsPreview, UGCFileDetailsOutputPreview, sort_keys=True, indent=4)
+
+if __name__ == '__main__':
+    download(sys.argv[1])
