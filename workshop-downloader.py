737ec54ec478e4c93d124bde5205168fa5c855f3
Blaze Alan Marshall <wertercatt@wertercatt.com>
2022-08-16 14:08:36 -0500

    Comments

--- workshop-downloader.py
+++ workshop-downloader.py
@@ -4,13 +4,17 @@ import datetime
 import requests
 import os
 
+#SteamWebAPI endpoints
 GetPublishedFileDetails = "https://api.steampowered.com/ISteamRemoteStorage/GetPublishedFileDetails/v1/"
 GetCollectionDetails = "https://api.steampowered.com/ISteamRemoteStorage/GetCollectionDetails/v1/"
 GetUGCFileDetails = "https://api.steampowered.com/ISteamRemoteStorage/GetUGCFileDetails/v1/"
+
+#Load SteamWebAPI Key
 KeyFile = open("./SteamWebAPI.key", "r")
 Key = KeyFile.read()
 KeyFile.close()
 
+#Load Workshop Page Details
 WorkshopID = str(sys.argv[1])
 GetPublishedFileDetailsData = {"key":Key,"itemcount":1,"publishedfileids[0]":WorkshopID}
 GetPublishedFileDetailsRaw = requests.post(url = GetPublishedFileDetails, data = GetPublishedFileDetailsData)
@@ -18,9 +22,13 @@ PublishedFileDetails = json.loads(GetPublishedFileDetailsRaw.text)["response"]["
 
 print(json.dumps(PublishedFileDetails, sort_keys=True, indent=4))
 
+#Get Variables for Output
 AppID = PublishedFileDetails["consumer_app_id"]
 SteamID64 = PublishedFileDetails["creator"]
 DateCreated = datetime.datetime(1970, 1, 1, tzinfo=datetime.timezone.utc) + datetime.timedelta(seconds=PublishedFileDetails["time_created"])
 DateCreatedISO8601 = DateCreated.strftime("%Y-%m-%dT%H-%M-%S")
+WorkshopTitle = PublishedFileDetails["title"]
+ItemFileName = PublishedFileDetails["filename"]
+ItemFileNameSplit = ItemFileName.split("/")
 
-print(DateCreatedISO8601)
+print(json.dumps(ItemFileNameSplit, sort_keys=True, indent=4))
